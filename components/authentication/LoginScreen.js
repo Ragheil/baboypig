b6a112a4074745431ada4278a6ffcafc04acfa25
Ragheil <atacadorragheil@gmail.com>
2024-09-04 20:54:46 +0800

    adjustded authentication UI

--- components/authentication/LoginScreen.js
+++ components/authentication/LoginScreen.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { sendPasswordResetEmail } from 'firebase/auth';
 import { auth } from '../../firebase/config2'; // Import auth here
+
+const { width } = Dimensions.get('window');
 
 const LoginScreen = ({ email, setEmail, password, setPassword, handleAuthentication, navigateToRegister }) => {
   const [passwordVisible, setPasswordVisible] = useState(false);
@@ -35,6 +37,9 @@ const LoginScreen = ({ email, setEmail, password, setPassword, handleAuthenticat
     return (
       <View style={styles.container}>
         <Text style={styles.header}>Forgot Password</Text>
+        <Text style={styles.instructions}>
+          Enter your email address below, and we'll send you an email with instructions to reset your password.
+        </Text>
         <TextInput
           style={styles.input}
           value={email}
@@ -42,9 +47,10 @@ const LoginScreen = ({ email, setEmail, password, setPassword, handleAuthenticat
           placeholder="Email Address"
           keyboardType="email-address"
           autoCapitalize="none"
+          placeholderTextColor="#666"
         />
-        <TouchableOpacity style={styles.button} onPress={handleForgotPassword}>
-          <Text style={styles.buttonText}>Send Reset Link</Text>
+        <TouchableOpacity style={styles.resetButton} onPress={handleForgotPassword}>
+          <Text style={styles.resetButtonText}>Send Reset Link</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => setForgotPassword(false)}>
           <Text style={styles.switchText}>Back to Login</Text>
@@ -60,9 +66,9 @@ const LoginScreen = ({ email, setEmail, password, setPassword, handleAuthenticat
       </View>
 
       <Text style={styles.header}>Login</Text>
-      <View style={styles.emailContainer}>
+      <View style={styles.inputContainer}>
         <TextInput
-          style={[styles.input, styles.emailInput]} // Applying custom style to the email input
+          style={styles.input}
           value={email}
           onChangeText={setEmail}
           placeholder="Email Address"
@@ -86,44 +92,69 @@ const LoginScreen = ({ email, setEmail, password, setPassword, handleAuthenticat
         </TouchableOpacity>
       </View>
 
-      <Text style={styles.orText}>or login with</Text>
-
-      <TouchableOpacity style={styles.googleButton}>
-        <Text style={styles.googleButtonText}>Google</Text>
+      <TouchableOpacity onPress={() => setForgotPassword(true)}>
+        <Text style={styles.toggleText}>Forgot Password?</Text>
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
         <Text style={styles.loginButtonText}>Login</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => setForgotPassword(true)}>
-        <Text style={styles.toggleText}>Forgot Password?</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={navigateToRegister}>
-        <Text style={styles.toggleText}>Register</Text>
+      <TouchableOpacity style={styles.registerButton} onPress={navigateToRegister}>
+        <Text style={styles.registerButtonText}>Register</Text>
       </TouchableOpacity>
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 20,
+    backgroundColor: '#F4F4F4',
+  },
+  headerBox: {
+    backgroundColor: '#A8C39F',
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 40,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: width, // Use screen width
+    height: 200,
+    zIndex: 1, // Ensure the header is above other content
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#000',
   },
   header: {
-    fontSize: 24,
+    fontSize: 22,
     fontWeight: 'bold',
     marginBottom: 20,
     textAlign: 'center',
+    color: '#000',
+    marginTop: 80
   },
-  input: {
+  instructions: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 20,
+    color: '#666',
+  },
+  inputContainer: {
     backgroundColor: '#fff',
     borderRadius: 8,
-    padding: 15,
     marginVertical: 10,
-    elevation: 2,
+    elevation: 4,
+  },
+  input: {
+    padding: 15,
     fontSize: 16,
   },
   passwordContainer: {
@@ -131,45 +162,63 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#fff',
     borderRadius: 8,
-    paddingHorizontal: 15,
     marginVertical: 10,
-    elevation: 2,
+    elevation: 4,
   },
   toggleIcon: {
     position: 'absolute',
     right: 15,
   },
-  button: {
-    backgroundColor: '#007bff',
+  resetButton: {
+    backgroundColor: '#000',
     borderRadius: 8,
     padding: 15,
     marginVertical: 10,
     alignItems: 'center',
   },
-  buttonText: {
+  resetButtonText: {
     color: '#fff',
     fontSize: 16,
     fontWeight: 'bold',
   },
-  switch: {
+  loginButton: {
+    backgroundColor: '#000',
+    borderRadius: 8,
+    padding: 15,
+    marginVertical: 10,
     alignItems: 'center',
-    marginTop: 20,
+    marginTop: 40,
+  },
+  loginButtonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  registerButton: {
+    backgroundColor: '#000000',
+    borderRadius: 8,
+    padding: 15,
+    marginVertical: 10,
+    alignItems: 'center',
+    elevation: 50,
+  },
+  registerButtonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  toggleText: {
+    textAlign: 'center',
+    marginVertical: 5,
+    color: '#000',
+    fontSize: 14,
+    marginTop: 15,
   },
   switchText: {
-    color: '#007bff',
+    textAlign: 'center',
+    marginTop: 15,
+    color: '#666',
     fontSize: 14,
-  },
-  googleButton: {
-    backgroundColor: '#4285F4',
-    borderRadius: 8,
-    padding: 15,
-    marginVertical: 10,
-    alignItems: 'center',
-  },
-  googleButtonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
   },
 });
 
--- components/authentication/RegisterScreen.js
+++ components/authentication/RegisterScreen.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+
+const { width } = Dimensions.get('window');
 
 const RegisterScreen = ({ email, setEmail, password, setPassword, handleAuthentication, navigateToLogin }) => {
   const [retypePassword, setRetypePassword] = useState('');
@@ -19,7 +21,11 @@ const RegisterScreen = ({ email, setEmail, password, setPassword, handleAuthenti
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Create your Account</Text>
+      <View style={styles.headerBox}>
+        <Text style={styles.title}>PigEx</Text>
+      </View>
+
+      <Text style={styles.header}>Create your account</Text>
       
       <View style={styles.nameContainer}>
         <TextInput
@@ -77,19 +83,19 @@ const RegisterScreen = ({ email, setEmail, password, setPassword, handleAuthenti
           <Ionicons name={retypePasswordVisible ? "eye-off" : "eye"} size={24} color="#333" />
         </TouchableOpacity>
       </View>
-      
+
       <TouchableOpacity
-        style={styles.button}
+        style={styles.submitButton}
         onPress={handleRegister}
       >
-        <Text style={styles.buttonText}>Register</Text>
+        <Text style={styles.submitButtonText}>Submit</Text>
       </TouchableOpacity>
       
       <TouchableOpacity
         onPress={navigateToLogin}
-        style={styles.switch}
+        style={styles.loginButton}
       >
-        <Text style={styles.switchText}>Already have an account? Login</Text>
+        <Text style={styles.loginButtonText}>Login</Text>
       </TouchableOpacity>
     </View>
   );
@@ -100,18 +106,35 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 20,
+    backgroundColor: '#F4F4F4',
+  },
+  headerBox: {
+    backgroundColor: '#A8C39F',
+    borderBottomLeftRadius: 40,
+    borderBottomRightRadius: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 30,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: width, // Use screen width
+    height: 170,
+    zIndex: 1, // Ensure the header is above other content
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#000',
+    textAlign: 'center',
   },
   header: {
-    fontSize: 24,
+    fontSize: 22,
     fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  input: {
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    marginVertical: 10,
-    elevation: 2,
+    marginBottom: 15,
+    textAlign: 'center',
+    color: '#000',
+    marginTop: 100,
   },
   nameContainer: {
     flexDirection: 'row',
@@ -119,35 +142,63 @@ const styles = StyleSheet.create({
   },
   halfInput: {
     flex: 1,
-    marginRight: 10,
+    marginRight: 5,
+  },
+  input: {
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    padding: 15,
+    marginVertical: 5,
+    elevation: 2,
   },
   passwordContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    marginVertical: 10,
+    elevation: 4,
   },
   toggleIcon: {
     position: 'absolute',
-    right: 10,
-    top: 10,
+    right: 15,
+    bottom: 20,
   },
-  button: {
-    backgroundColor: '#007bff',
-    borderRadius: 5,
+  submitButton: {
+    backgroundColor: '#000',
+    borderRadius: 8,
     padding: 15,
-    marginVertical: 10,
+    marginVertical: 5,
     alignItems: 'center',
+    marginTop: 30,
+    elevation: 4, // Add elevation for Android shadow
+    shadowColor: '#000', // iOS shadow
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
   },
-  buttonText: {
+  submitButtonText: {
     color: '#fff',
     fontSize: 16,
     fontWeight: 'bold',
   },
-  switch: {
+  loginButton: {
+    backgroundColor: '#000',
+    borderRadius: 8,
+    padding: 15,
+    marginVertical: 5,
     alignItems: 'center',
-    marginTop: 20,
+    marginTop: 10,
+    elevation: 4, // Add elevation for Android shadow
+    shadowColor: '#000', // iOS shadow
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
   },
-  switchText: {
-    color: '#007bff',
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
